489ea9d9114c38ebe5f3cf5f1d055d740a38fa92
Jeancarlo <2423110340@untels.edu.pe>
2025-05-10 18:46:42 -0500

    Proyecto temu parte 2

--- script.js
+++ script.js
@@ -12,4 +12,20 @@ function hello() {
     }, 5000);
     
 }
-btn.addEventListener("click", hello)
+btn.addEventListener("click", hello);
+
+const imput = document.getElementById("imput");
+
+imput.addEventListener("input", (e) => {
+    console.log(e.target.value);
+    title.innerText = e.target.value;
+});
+
+const prompt = document.getElementById("prompt");
+const generate = document.getElementById("generate");
+const image = document.getElementById("image");
+
+generate.addEventListener("click", () => {
+    const imgSrc = "https://image.pollinations.ai/prompt/" + prompt.value;
+    image.src = imgSrc;
+});
